18a55c034492f203c22f4b5eb51adb293b30bfbc
Luk Krzy <lukaskrzy@gmail.com>
2021-08-10 20:29:48 +0200

    Fix patch cors

--- src/main/java/com/driva/drivaapi/security/Cors.java
+++ src/main/java/com/driva/drivaapi/security/Cors.java
@@ -27,7 +27,7 @@ public class Cors {
 			  Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
 							"Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
 							"Access-Control-Allow-Credentials"));
-	  corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	  corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"));
 	  urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 	  return new CorsFilter(urlBasedCorsConfigurationSource);
    }
